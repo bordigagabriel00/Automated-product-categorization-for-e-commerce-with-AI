1f315ae71548922ff1c5157847d358edef6ef36a
rcastillo <tepuybits@gmail.com>
2024-02-28 02:06:32 -0300

    Update: Build UI

---
+++ src/webui/ui/static/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "../templates/**/*.{html,js}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("@tailwindcss/typography"),require("daisyui")],
+
+  daisyui: {
+    themes: ["synthwave","night"],
+  },
+}
