86556fc84c140d38dfe4b638494fdd459f5d9789
rcastillo <tepuybits@gmail.com>
2024-02-29 23:25:30 -0300

    Update: Check Activate flow

--- infra/prefect/flow.py
+++ infra/prefect/flow.py
@@ -1,32 +1,17 @@
-from prefect import flow, task
-from prefect.deployments import Deployment
+from prefect import flow, task, get_run_logger
+import asyncio
 
-from datetime import timedelta
 
 
 @task
-def say_hello():
-    print("Hello, World!")
+async def activate_task():
+    print("Hello, Prefect!")
 
 
 @flow
-def hello_world_flow():
-    say_hello()
-
-
-# Deployment
-# Define an interval schedule to run the flow automatically, if needed.
-# schedule = IntervalSchedule(interval=timedelta(hours=1))  # Example: run every hour.
+async def activate_flow():
+    logger = get_run_logger()
+    await activate_task()
 
 if __name__ == "__main__":
-    # Deploy the flow to Prefect Server
-    deployment = Deployment.build_from_flow(
-        flow=hello_world_flow,
-        name="Hello World Flow Deployment",
-        work_queue_name="TEST_WORK_QUEUEE",
-        # schedule=schedule,  # Omit or modify this if you don't want a schedule
-    )
-    deployment.apply()
-
-    # run the flow immediately
-    hello_world_flow()
+    asyncio.run(activate_flow())
