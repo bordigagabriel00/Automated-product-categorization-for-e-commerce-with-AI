0a6143a384dca95143185eb8ab7b9a205cd05fd9
rcastillo <tepuybits@gmail.com>
2024-03-18 18:38:20 -0300

    Update: Model Predict

--- src/inference/api/simulator/consumer.py
+++ src/inference/api/simulator/consumer.py
@@ -1,39 +1,56 @@
 import json
 import logging
+from typing import Any
 
-from api.simulator.model import ResponsePrediction
+from api.simulator.model import ResponsePrediction, PredictRequest
 from core.eventbus import nats_provider
 from core.eventbus import topic_predict_response
+from core.model_ai_provider import model_provider
+from core.normalization_provider import normalize_text
+
+
+def text_field_normalize(request: PredictRequest):
+    request.normalized_name = normalize_text(request.name)
+    request.normalized_description = normalize_text(request.description)
+
+
+def init_prediction_request(data: Any) -> PredictRequest:
+    pred_request = PredictRequest(id="")
+    pred_request.id = data['id']
+    pred_request.name = data['name']
+    pred_request.description = data['description']
+    pred_request.price = data['price']
+    pred_request.product_type = data['product_type']
+    pred_request.manufacturer = data['manufacturer']
+
+    return pred_request
 
 
 async def predict_request_handler(msg):
+    logging.info(f"Predict: Starting process")
     data = json.loads(msg.data.decode())
-    logging.info(f"Received subject request: {msg.subject} ")
-    logging.info(f"Received predict request: {data}")
+    logging.info(f"Predict: Received subject request: {msg.subject} ")
+    logging.info(f"Predict: Received predict request: {data}")
 
-    data_dummy = [
-        {
-            "category": "Electronics",
-            "subcategories": [
-                {
-                    "category": "Computers",
-                    "subcategories": [
-                        {
-                            "category": "Laptops",
-                            "subcategories": [
-                                {
-                                    "category": "Gaming",
-                                    "subcategories": [
-                                        {"category": "High-End", "level": "5"}
-                                    ]
-                                }]
-                        }, ]
-                }]
-        }
-    ]
-    logging.info(f"Predict process")
+    categories = ["Category 1", "Category 2", "Category 3", "Category 4", "Category 5"]
+    payload = {
+        "prediction_id": data["id"],
+        "categories": categories
 
-    resp_prediction = ResponsePrediction(id=data["id"], payload=json.dumps(data_dummy))
+    }
+
+    model_1 = model_provider.get_model("model1")
+    if model_1:
+        logging.info("Model 1 loaded and ready for use.")
+    else:
+        logging.error("Model 1 could not be loaded.")
+
+    # pred_request = init_prediction_request(data)
+    # text_field_normalize(pred_request)
+
+    logging.info(f"Predict: Ending process")
+
+    resp_prediction = ResponsePrediction(id=data["id"], payload=json.dumps(payload))
     logging.info(f"Predict result: {resp_prediction}")
     try:
 
