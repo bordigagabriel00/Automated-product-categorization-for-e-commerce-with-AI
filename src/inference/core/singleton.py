efcdea27c13e51ebf54dadaafce6c1f842bb35c6
rcastillo <tepuybits@gmail.com>
2024-03-21 03:32:14 -0300

    Add Singleton

---
+++ src/inference/core/singleton.py
@@ -0,0 +1,10 @@
+class SingletonMeta(type):
+    """
+    A metaclass that creates a Singleton instance.
+    """
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super().__call__(*args, **kwargs)
+        return cls._instances[cls]
