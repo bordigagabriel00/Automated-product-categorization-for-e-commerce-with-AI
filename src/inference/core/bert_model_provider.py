53658daf703fa68df687032a1bad4a9af31f4417
bordigagabriel00 <96091770+bordigagabriel00@users.noreply.github.com>
2024-03-24 22:18:24 -0300

    bert_model_provider.py

--- src/inference/core/bert_model_provider.py
+++ src/inference/core/bert_model_provider.py
@@ -4,13 +4,22 @@ from transformers import BertTokenizer, TFBertModel, PreTrainedTokenizerBase
 
 from core.logger_provider import logger
 
-
+# Creating variables to store the tokenizer, BERT model, and loading state:
 class BertService:
     def __init__(self):
         self.tokenizer = None
         self.bert_model = None
         self.is_loaded = False
-
+        
+    """
+    Description 'init_load_bert_model':
+    - Records the start of asynchronous loading of the tokenizer and the BERT model.
+    - Asynchronous loading of the BERT tokenizer to not block execution.
+    - Asynchronous loading of the BERT model, similar to the tokenizer.
+    - Confirm through the logger that the model and tokenizer were loaded correctly.
+    - In case of an error during charging, it records it and marks the charging status as false.
+    """
+    
     async def init_load_bert_model(self):
         try:
             logger.info("Starting to load the BERT model and tokenizer asynchronously")
@@ -22,11 +31,13 @@ class BertService:
             logger.error(f"An error occurred while loading the BERT model and tokenizer: {e}")
             self.is_loaded = False
 
+    # Access to loaded BERT tokenizer:
     def get_tokenizer(self):
         return self.tokenizer
 
+    # Access to loaded BERT model:
     def get_bert_model(self):
         return self.bert_model
 
-
+# Model loading initialization and BERT tokenizer:
 bert_service = BertService()
