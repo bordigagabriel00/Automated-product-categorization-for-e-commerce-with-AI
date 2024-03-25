86dea30679383668fc267d38efce1b63df943f20
rcastillo <tepuybits@gmail.com>
2024-03-25 00:09:46 -0300

    Update: Docker, Docker compose, setting

--- src/inference/core/normalization_provider.py
+++ src/inference/core/normalization_provider.py
@@ -27,6 +27,8 @@ async def init_normalization():
     nltk.download("stopwords")
     nltk.download("wordnet")
     nltk.download("omw-1.4")
+    nltk.download('punkt')
+    nltk.download('averaged_perceptron_tagger')
 
     stop_words = set(stopwords.words('english'))
     lemmatizer = WordNetLemmatizer()
